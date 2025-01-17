86c8dd71af45fb61b6d0083e5d2857230b24302b
lucas_hub12 <hyeonggyunbae@gmail.com>
2025-01-17 15:33:54 +0900

    build

--- prd-build.sh
+++ prd-build.sh
@@ -2,4 +2,7 @@
 pnpm docs:build
 cp CNAME docs/
 cp robots.txt docs/
-# cp naver7b7f7dc6cce8eb9498e18ebf4ae99fc9.html docs/
+cp naver569b2e7cebefad7b11c739ef3b22839d.html docs/
+git add .
+git commit -m "build"
+git push
